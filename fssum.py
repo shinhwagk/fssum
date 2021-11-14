e9faee71045f19d017ff5016e91c3fbca77c9785
gk <shinhwagk@outlook.com>
2021-11-14 14:36:57 +0800

    v2

--- fssum.py
+++ fssum.py
@@ -4,19 +4,7 @@ import os
 import sys
 import hashlib
 
-sampleSize = 16 * 1024
 sampleThreshold = 128 * 1024
-
-
-def medianOffset(fsize):
-    rsize = 0
-    offset = sampleSize
-    if fsize % 2 == 0:
-        rsize = fsize / 2 - sampleSize / 2
-    else:
-        rsize = (fsize - 1) / 2 - sampleSize / 2
-        offset += 1
-    return (int(rsize), offset)
 
 
 def hashCore(fstat, fio):
@@ -24,24 +12,22 @@ def hashCore(fstat, fio):
     if fstat.st_size < sampleThreshold:
         shasum.update(fio.read())
     else:
-        shasum.update(fio.read(sampleSize))
-        (r, o) = medianOffset(fstat.st_size)
-        fio.seek(r)
-        shasum.update(fio.read(o))
-        fio.seek(-1*sampleSize, os.SEEK_END)
+        stepSize = int(fstat.st_size / step)
+        for i in range(0, step-1):
+            fio.seek(i*stepSize)
+            shasum.update(fio.read(1))
+        fio.seek(-1, os.SEEK_END)
         shasum.update(fio.read())
     return shasum.hexdigest()
 
 
 def main():
-    try:
-        if len(sys.argv) > 1:
-            fstat = os.stat(sys.argv[1])
-            with open(sys.argv[1], 'rb') as fio:
-                print(hashCore(fstat, fio))
-    except BaseException as e:
-        print(e)
+    if len(sys.argv) > 1:
+        fstat = os.stat(sys.argv[1])
+        with open(sys.argv[1], 'rb') as fio:
+            print(hashCore(fstat, fio))
 
 
 if __name__ == "__main__":
+    step = int(os.getenv('fssum_step') or "8")
     main()
